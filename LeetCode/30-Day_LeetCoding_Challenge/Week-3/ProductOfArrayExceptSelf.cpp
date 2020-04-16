99a8a9a8ba9fd1e00d117a1766be17fbc2781919
phoenixx1 <xphoenix1010@gmail.com>
2020-04-16 18:22:27 +0530

    Modified Solution

--- LeetCode/30-Day_LeetCoding_Challenge/Week-3/ProductOfArrayExceptSelf.cpp
+++ LeetCode/30-Day_LeetCoding_Challenge/Week-3/ProductOfArrayExceptSelf.cpp
@@ -1,24 +1,29 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> ans;
-        for(int i=0; i<nums.size(); i++){
-            int temp = nums[i];
-            auto it = nums.begin() + i;
-            nums.erase(it);
-            ans.push_back(accumulate(nums.begin(), nums.end(), 1, multiplies<int>()));
-            nums.insert(it, temp);
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        int product = 1, zeroCount = 0, prodOfNZero = 1;
+        for(int x : nums){
+            product *= x;
+            if(x == 0){
+                zeroCount++;
+            }else{
+                prodOfNZero *= x;
+            }
+        }
+        if(product != 0){
+            for(int i=0; i<nums.size(); i++){
+                ans[i] = (product / nums[i]);
+            }
+        }else if(zeroCount == 1){
+            for(int i=0; i<nums.size(); i++){
+                if(nums[i] == 0){
+                    ans[i] = prodOfNZero;
+                }
+            }
+        }else{
+            //nothing
         }
         return ans;
     }
-
-int main(){
-    vector<int> nums = {1,2,3,4};
-    nums = productExceptSelf(nums);
-    for(int i=0; i<nums.size(); i++){
-        cout << nums[i] << " ";
-    }
-    cout << endl;
-    return 0;
-}
+};
